2d7ab19aeb951799f100d956789c13b43f3dc37c
jsteryous <jsteryous@example.com>
2026-01-08 17:43:20 -0500

    supabase

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase Configuration
+const SUPABASE_CONFIG = {
+    url: 'https://ykuenmwfxecmmqichwit.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrdWVubXdmeGVjbW1xaWNod2l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MTAxMzAsImV4cCI6MjA4MzQ4NjEzMH0.MoJO92cIHwVXKGj7A9NXtCZW-JaKKAPrxxoch_Ga1Qk'
+};
